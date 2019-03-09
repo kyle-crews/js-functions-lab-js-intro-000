baa8e1cf370575df5e22a984f3506b9b9406e31c
Kyle Crews <kyledcrews@gmail.com>
2019-03-09 15:39:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function happyHolidayTo(holiday, name) {
   return 'Happy Independence Day, you!';
 }
 
+var holiday = 'Mother\'s Day'
+var days = '20'
 function holidayCountdown(holiday, days) {
   return 'It\'s ${days} days until ${holiday}!'
 }
