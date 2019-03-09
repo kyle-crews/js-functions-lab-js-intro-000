c0f4efbb851f841e9bb9b94b7ec9c3d8f3b38439
Kyle Crews <kyledcrews@gmail.com>
2019-03-09 15:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  var name = you
-  return "Happy holidays, ${name}"
+  return "Happy holidays, ${name}!"
 }
